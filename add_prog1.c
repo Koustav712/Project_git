ac0b082e7f28dc2c398fcd848e7609902854287a
Koustav712 <koustavdeydey@gmail.com>
2024-09-24 16:45:11 +0530

    Add files via upload

---
+++ add_prog1.c
@@ -0,0 +1,15 @@
+// write a program to find the addition of two numbers
+
+
+#include<stdio.h>
+int main()
+{
+	int num1,num2,add;
+	printf("The value of first number : ");
+	scanf("%d",&num1);
+	printf("The value of second number : ");
+	scanf("%d",&num2);
+	add=num1+num2;
+	printf("The addition of two numbers is : %d",add);
+	return 0;
+}
